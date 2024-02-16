cb3ec4ed26c1b4be9bb5274489c119279d224f9e
ZenithVal <88603991+ZenithVal@users.noreply.github.com>
2024-02-16 07:04:26 -0500

    Update Transcriber.py

--- Transcriber.py
+++ Transcriber.py
@@ -127,8 +127,9 @@ def create_lua_data(transcripts, settings):
     lua_data += "RecMedia = RecMedia or {}\n\n"
     
     for transcript_name, transcript_data in transcripts.items():
+        lua_data += f'-- {transcript_data["title"]}\n'
         lua_data += f'RecMedia["{transcript_name}"] = {{\n'
-        lua_data += f'\titemDisplayName = "{transcript_data["itemDisplayName"]}",\n'
+        lua_data += f'\titemDisplayName = "VHS: {transcript_data["itemDisplayName"]}",\n'
         lua_data += f'\ttitle = "{transcript_data["title"]}",\n'
         lua_data += f'\tsubtitle = {transcript_data.get("subtitle", "nil")},\n'
         lua_data += f'\tauthor = {transcript_data.get("author", "nil")},\n'
@@ -139,8 +140,8 @@ def create_lua_data(transcripts, settings):
         for line in transcript_data["lines"]:
             if line["text"]:  # Skip empty lines
                 lua_data += f'\t\t{{ text = "{line["text"]}", r = {line["color"][0]}, g = {line["color"][1]}, b = {line["color"][2]}, codes = "{line["codes"]}" }},\n'
-        lua_data += '\t}\n'
-        lua_data += '}\n\n'
+        lua_data += '\t},\n'
+        lua_data += '};\n\n'
     
     with open("Generated.lua", "w") as lua_file:
         lua_file.write(lua_data)
